153736b1ea27a59ff29f17e86730266ef2561707
17727921123 <caiang.666>
2019-12-03 11:49:56 +0800

    封装mybatis通用单表增删改查

--- src/main/java/entity/app/AppEntity.java
+++ src/main/java/entity/app/AppEntity.java
@@ -2,18 +2,18 @@ package entity.app;
 
 
 import annotation.Column;
-import annotation.CustomAnnotation;
 import annotation.Id;
 import annotation.Table;
 
 /**
  * 自定义注解
  */
-//@CustomAnnotation("entity.app.AppEntity")
 @Table(value = "app")
 public class AppEntity {
     private Integer id;
     private String appName;
+    private String appInfo;
+    private String appKey;
 
     @Id(value = "ID")
     public Integer getId() {
@@ -35,11 +35,33 @@ public class AppEntity {
         return this;
     }
 
+    @Column(value = "APP_INFO")
+    public String getAppInfo() {
+        return appInfo;
+    }
+
+    public AppEntity setAppInfo(String appInfo) {
+        this.appInfo = appInfo;
+        return this;
+    }
+
+    @Column(value = "APP_KEY")
+    public String getAppKey() {
+        return appKey;
+    }
+
+    public AppEntity setAppKey(String appKey) {
+        this.appKey = appKey;
+        return this;
+    }
+
     @Override
     public String toString() {
         return "AppEntity{" +
                 "id=" + id +
                 ", appName='" + appName + '\'' +
+                ", appInfo='" + appInfo + '\'' +
+                ", appKey='" + appKey + '\'' +
                 '}';
     }
 }

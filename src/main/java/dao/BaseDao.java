0fe46d00b85f37dc0b8bd6f7d9183a46a7f80f0c
17727921123 <caiang.666>
2019-12-03 18:04:38 +0800

    封装mybatis通用单表增删改查

---
+++ src/main/java/dao/BaseDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+
+import java.util.HashMap;
+
+public interface BaseDao {
+    Integer save(Object object);
+
+    Integer remove(Object object);
+
+    Integer update(Object object);
+
+    HashMap query(Integer id, Class c);
+}

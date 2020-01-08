d79ff8807644d1046a8d6f8ad6ebd95ded69e279
17727921123 <caiang.666>
2020-01-08 15:59:38 +0800

    自定义注解+AOP 采集日志

--- src/main/java/com/lsm/common/aop/WebLogAspect.java
+++ src/main/java/com/lsm/common/aop/WebLogAspect.java
@@ -1,18 +1,24 @@
 package com.lsm.common.aop;
 
+import com.alibaba.fastjson.JSON;
 import com.google.gson.Gson;
 import com.lsm.common.annotation.WebLog;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
+import java.util.*;
 
 @Aspect
 @Component
@@ -45,13 +51,20 @@ public class WebLogAspect {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
         // 获取 @WebLog 注解的描述信息
-        String methodDescription = getAspectLogDescription(joinPoint);
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        WebLog webLog = method.getAnnotation(WebLog.class);
+        String description = webLog.description();
+        int level = webLog.level();
+        String type = webLog.type().getType();
         // 打印请求相关参数
         logger.info("========================================== Start ==========================================");
         // 打印请求 url
         logger.info("URL            : {}", request.getRequestURL().toString());
         // 打印描述信息
-        logger.info("Description    : {}", methodDescription);
+        logger.info("description    : {}", description);
+        logger.info("level          : {}", level);
+        logger.info("type           : {}", type);
         // 打印 Http method
         logger.info("HTTP Method    : {}", request.getMethod());
         // 打印调用 controller 的全路径以及执行方法
@@ -59,7 +72,15 @@ public class WebLogAspect {
         // 打印请求的 IP
         logger.info("IP             : {}", request.getRemoteAddr());
         // 打印请求入参
-        logger.info("Request Args   : {}", new Gson().toJson(joinPoint.getArgs()));
+        Object[] args = joinPoint.getArgs();
+        ParameterNameDiscoverer pnd = new DefaultParameterNameDiscoverer();
+        Gson gson = new Gson();
+        String[] parameterNames = pnd.getParameterNames(method);
+        Map<String, Object> paramMap = new HashMap<>(32);
+        for (int i = 0; i < parameterNames.length; i++) {
+            paramMap.put(parameterNames[i], gson.toJson(args[i]));
+        }
+        logger.info("Request Args   : {}", paramMap);
     }
 
     /**
@@ -92,29 +113,20 @@ public class WebLogAspect {
     }
 
     /**
-     * 获取切面注解的描述
+     * 处理完请求，返回内容
      *
-     * @param joinPoint 切点
-     * @return 描述信息
-     * @throws Exception
+     * @param result
      */
-    public String getAspectLogDescription(JoinPoint joinPoint)
-            throws Exception {
-        String targetName = joinPoint.getTarget().getClass().getName();
-        String methodName = joinPoint.getSignature().getName();
-        Object[] arguments = joinPoint.getArgs();
-        Class targetClass = Class.forName(targetName);
-        Method[] methods = targetClass.getMethods();
-        StringBuilder description = new StringBuilder();
-        for (Method method : methods) {
-            if (method.getName().equals(methodName)) {
-                Class[] clazz = method.getParameterTypes();
-                if (clazz.length == arguments.length) {
-                    description.append(method.getAnnotation(WebLog.class).description());
-                    break;
-                }
-            }
-        }
-        return description.toString();
+    @AfterReturning(returning = "result", pointcut = "webLog()")
+    public void doAfterReturning(Object result) {
+        logger.info("方法的返回值             : {}", result);
+    }
+
+    /**
+     * 后置异常通知
+     */
+    @AfterThrowing("webLog()")
+    public void throwException(JoinPoint joinPoint) {
+        logger.info("方法异常时执行.....");
     }
 }
